3ffb2ddd58990d652775497d6ef16c7adc46f3cb
sairamsarath <47963990+sairamsarath@users.noreply.github.com>
2026-01-20 20:45:58 +0530

    Update file.js

--- file.js
+++ file.js
@@ -1,10 +1,11 @@
 const name = "Sai Ram Sarath"
 const age = 28
 const city = "Hyderabad"
+const country = "India"
 
 function greet()
 {
-    console.log(`My name is ${name} and age is ${age} from ${city}`)
+    console.log(`My name is ${name} and age is ${age} from ${city} and ${country}`)
 }
 
 greet()
